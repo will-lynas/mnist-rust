fcb8e90702daf0e0d7f5172f2fab1fc49c0d76d2
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-28 13:22:11 +0000

    Turn into a lib

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,5 @@
-use std::iter::zip;
-
-use ndarray::Array1;
-use ndarray::Array2;
+use mnist_rust::Network;
 use ndarray::array;
-use ndarray_rand::RandomExt;
-use ndarray_rand::rand_distr::Normal;
-
-fn sigmoid(x: f64) -> f64 {
-    1.0 / (1.0 + (-x).exp())
-}
-
-#[allow(dead_code)]
-struct Network {
-    num_layers: usize,
-    sizes: Vec<usize>,
-    biases: Vec<Array1<f64>>,
-    weights: Vec<Array2<f64>>,
-}
-
-impl Network {
-    fn new(sizes: Vec<usize>) -> Self {
-        let num_layers = sizes.len();
-        let biases = sizes
-            .iter()
-            .skip(1)
-            .map(|&size| Array1::random(size, Normal::new(0.0, 1.0).unwrap()))
-            .collect();
-        let weights = zip(sizes.iter(), sizes.iter().skip(1))
-            .map(|(&x, &y)| Array2::random((y, x), Normal::new(0.0, 1.0).unwrap()))
-            .collect();
-
-        Network {
-            num_layers,
-            sizes,
-            biases,
-            weights,
-        }
-    }
-
-    fn feedforward(&self, a: Array1<f64>) -> Array1<f64> {
-        zip(self.biases.iter(), self.weights.iter())
-            .fold(a, |a, (b, w)| (w.dot(&a) + b).mapv(sigmoid))
-    }
-}
 
 fn main() {
     let network = Network::new(vec![3, 4, 4, 3]);
