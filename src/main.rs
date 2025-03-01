e0c18ded942cbb06bffcc6e08906decea73e8aa8
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-01 00:49:11 +0000

    Use the network

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use mnist_rust::{Network, load_mnist_samples};
-use ndarray::array;
 
 fn main() {
-    let network = Network::new(vec![3, 4, 4, 3]);
-    let a = array![1.0, 2.0, 3.0];
-    let b = network.feedforward(&a);
-    println!("{:?}", b);
-
     let mnist_train = load_mnist_samples("data/mnist_train_processed.bin");
-    println!("{:?}", mnist_train.len());
+    let mnist_test = load_mnist_samples("data/mnist_test_processed.bin");
+
+    let mut network = Network::new(vec![28 * 28, 30, 10]);
+    let epochs = 30;
+    let mini_batch_size = 10;
+    let eta = 3.0;
+    network.sgd(
+        mnist_train.iter().collect(),
+        epochs,
+        mini_batch_size,
+        eta,
+        Some(&mnist_test),
+    );
 }
